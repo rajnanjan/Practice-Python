d4166c17c678fe31297f0ae597b114ed75cc879c
rajnanjan <postbox1507@gmail.com>
2021-11-09 09:52:38 +0530

    while looping

---
+++ forloop.py
@@ -0,0 +1,8 @@
+''' for loop '''
+print(233 % 7.9)
+for i in range(21):
+
+    if i % 2 != 0:
+        print(i)
+
+''' '''
---
+++ while.py
@@ -0,0 +1,6 @@
+num = 20
+
+while num > 0:
+    if num % 2 == 0:
+        print(num)
+    num -= 1
