43b5865df82142a9d40815f0da2500c75a14492a
rajnanjan <postbox1507@gmail.com>
2021-11-09 18:33:26 +0530

    pratice programs

--- forloop.py
+++ forloop.py
@@ -1,8 +1,35 @@
-''' for loop '''
-print(233 % 7.9)
-for i in range(21):
+# ''' for loop '''
+# print(233 % 7.9)
+# for i in range(21):
+#
+#     if i % 2 != 0:
+#         print(i)
 
-    if i % 2 != 0:
-        print(i)
+""" 1- Write a program to print the following using for loop
+     a. First 10 Even numbers
+     b. First 10 Odd numbers
+     c. First 10 Natural numbers
+     d. First 10 Whole numbers """
+# for num in range(0, 20, 2):
+#     print(num)
 
-''' '''
+# for num in range(1, 20,2):
+#     print(num)
+
+# for num in range(1, 11):
+#     print(num)
+#
+# for num in range(0, 10):
+#     print(num)
+""" 2-Write a program to print first 10 integers and their squares like
+1 1
+2 4
+3 9 and so on"""
+
+# print("numbers\t\t\tsquares")
+# for num in range(1, 11):
+#     print(num, "\t\t\t", num ** 2)
+
+""" Write for loop statement to print the following series:
+10, 20, 30 … … 300"""
+
