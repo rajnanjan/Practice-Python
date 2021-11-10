262e4463b64a0e6496f9c682fcaf53e52a16536c
rajnanjan <postbox1507@gmail.com>
2021-11-10 12:17:00 +0530

    day 2 pratice program

--- while.py
+++ while.py
@@ -1,6 +1,94 @@
-num = 20
+# num = 20
+#
+# while num > 0:
+#     if num % 2 == 0:
+#         print(num)
+#     num -= 1
 
-while num > 0:
-    if num % 2 == 0:
-        print(num)
-    num -= 1
+# 1- Write a program in Python to display the Factorial of a number. "
+# num = int(input("enter the number "))
+# #
+# if num == 0 or num < 0:
+#     print("given number is should be greater than 1")
+# else:
+#     fact_num = 1
+#     while num > 0:
+#         fact_num *= num
+#         num -= num
+#         print("factorial number is ", fact_num)
+#  written by sachin
+# n = int(input("Enter the number: "))
+# if n < 0:
+#     print("No Negative Values")
+#
+# else:
+#     result = 1
+#
+#     while n > 0:
+#         result *= n
+#         n -= 1
+#
+#     print(result)
+
+"""Q1. Write a program to print the following using while loop
+a. First 10 Even numbers
+b. First 10 Odd numbers
+c. First 10 Natural numbers
+d. First 10 Whole numbers"""
+# num = 2
+#
+# while num <= 20:
+#     print(num)
+#     num += 2
+#
+#
+# num = 1
+# while num < 20:
+#     print(num)
+#     num += 2
+#
+# num = 1
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
+#
+# num = 0
+#
+# while num < 10:
+#     print(num)
+#     num += 1
+""" Q2. Write a program to print first 10 integers and their squares using while loop.
+1 1
+2 4
+3 9 and so on """
+
+# num = 1
+# print("number \t\t\t\t squares")
+# while num <= 10:
+#     print(num, "\t\t\t", num ** 2)
+#     num += 1
+
+""" Write a while loop statement to print the following series
+105, 98, 91 ………7."""
+
+# num = 105
+#
+# while num != 0 and num <= 105:
+#     print(num, end=",")
+#     num -= 7
+""" Write a program to print first 10 natural number in reverse order using while loop."""
+#
+# num = 10
+# while num >= 1:
+#     print(num)
+#     num -= 1
+""" Write a program to print sum of first 10 Natural numbers."""
+
+# num = 1
+# n = 0
+# while num <= 10:
+#     n += num
+#     num += 1
+# print("sum of first natural numbers :", n)
+
