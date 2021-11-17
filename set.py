9a611fb22587d9c5af79a490aa90182533ab70dd
rajnanjan <postbox1507@gmail.com>
2021-11-18 00:18:03 +0530

    using the remove funtion

--- set.py
+++ set.py
@@ -8,8 +8,10 @@
 # print(fruits)
 # print(type(fruits))
 """3. Write a Python program to add member(s) in a set."""
+# values = {"apple", "orange", "mango"}
+# values.add("banana")
+# print(values)
+""" 4. Write a Python program to remove item(s) from set """
 values = {"apple", "orange", "mango"}
-values.add("banana")
+values.remove("mango")
 print(values)
-
-
