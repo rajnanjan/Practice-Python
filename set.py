3e3770a621a8a92a7eaa3c307f14740f9d87e240
rajnanjan <postbox1507@gmail.com>
2021-11-18 00:09:32 +0530

    using the iteration

--- set.py
+++ set.py
@@ -1,4 +1,10 @@
 """ 1. Write a Python program to create a set."""
+# values = {"apple","orange","mango"}
+# print(values)
+# print(type(values))
+""" 2. Write a Python program to iteration over sets."""
 values = {"apple","orange","mango"}
-print(values)
-print(type(values))
+fruits = set(values)
+print(fruits)
+print(type(fruits))
+
