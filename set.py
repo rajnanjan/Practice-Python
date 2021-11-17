a11ba64ddf247f43a6e036e39d1b621c865a2abd
rajnanjan <postbox1507@gmail.com>
2021-11-18 00:14:27 +0530

    using the add function

--- set.py
+++ set.py
@@ -3,8 +3,13 @@
 # print(values)
 # print(type(values))
 """ 2. Write a Python program to iteration over sets."""
-values = {"apple","orange","mango"}
-fruits = set(values)
-print(fruits)
-print(type(fruits))
+# values = {"apple","orange","mango"}
+# fruits = set(values)
+# print(fruits)
+# print(type(fruits))
+"""3. Write a Python program to add member(s) in a set."""
+values = {"apple", "orange", "mango"}
+values.add("banana")
+print(values)
+
 
