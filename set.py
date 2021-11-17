6f2edbb3a8c6b2f892e835ceb7c268af1ab31717
rajnanjan <postbox1507@gmail.com>
2021-11-18 00:35:20 +0530

    using the union function

--- set.py
+++ set.py
@@ -20,9 +20,13 @@
 # num.discard(56)
 # print(num)
 """ 6. Write a Python program to create an intersection of sets."""
+# roll_no = {1, 2, 3, 4, 5, 6, 7, 8}
+# rank_no ={12, 4, 6, 7, 3, 4}
+# common_no = roll_no.intersection(rank_no)
+# print(common_no)
+# print(common_no)
+""" 7. Write a Python program to create a union of set"""
 roll_no = {1, 2, 3, 4, 5, 6, 7, 8}
 rank_no ={12, 4, 6, 7, 3, 4}
-common_no = roll_no.intersection(rank_no)
+common_no = roll_no.union(rank_no)
 print(common_no)
-print(common_no)
-
