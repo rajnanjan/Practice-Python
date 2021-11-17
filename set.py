a140a5d0abbe6ea9c2f1ed2f66354eb1949e1c63
rajnanjan <postbox1507@gmail.com>
2021-11-18 00:24:40 +0530

    using the discard function

--- set.py
+++ set.py
@@ -12,6 +12,10 @@
 # values.add("banana")
 # print(values)
 """ 4. Write a Python program to remove item(s) from set """
-values = {"apple", "orange", "mango"}
-values.remove("mango")
-print(values)
+# values = {"apple", "orange", "mango"}
+# values.remove("mango")
+# print(values)
+""" 5. Write a Python program to remove an item from a set if it is present in the set. """
+num = {1, 4, 5, 8, 6, 56, 78}
+num.discard(56)
+print(num)
