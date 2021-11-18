14f4e17a065da0d11833f9d17f6ff52e9daa5a59
rajnanjan <postbox1507@gmail.com>
2021-11-18 20:34:35 +0530

    multiplies all the items in list

--- list.py
+++ list.py
@@ -1,9 +1,16 @@
 """ 1. Write a Python program to sum all the items in a list. """
+# num = [1, 3, 5, 6, 7, 13, 56]
+# sum_of_num = 0
+# for i in num:
+#     sum_of_num = sum_of_num + i
+# print("Sum of all items :", sum_of_num)
+""" 2. Write a Python program to multiplies all the items in a list."""
 num = [1, 3, 5, 6, 7, 13, 56]
-sum_of_num = 0
+sum_of_num = 1
 for i in num:
-    sum_of_num = sum_of_num + i
-print("Sum of all items :", sum_of_num)
+    sum_of_num *= i
+print(sum_of_num)
+
 
 
 
