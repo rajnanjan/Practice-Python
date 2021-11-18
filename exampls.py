aa9aba1c4d188a7af040c39ca5eb3ed36a1d0765
rajnanjan <postbox1507@gmail.com>
2021-11-18 10:19:52 +0530

    example programs

---
+++ exampls.py
@@ -0,0 +1,42 @@
+"""" 1. Write a Python program which accepts the users first and last name and
+print them in reverse order with a space between them."""
+# first_name = input("Enter your first name :")
+# last_name = input("Enter the last name :")
+#
+# print(last_name + " "+first_name)
+"""2. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. 
+Sample data : 3, 5, 7, 23 
+Output : 
+List : ['3', ' 5', ' 7', ' 23'] 
+Tuple : ('3', ' 5', ' 7', ' 23') """
+# numbers = input("enter the numbers :")
+#
+# num = numbers.split(",")
+# unique = tuple(num)
+# print(num)
+# print(unique)
+# print(type(num))
+# print(type(unique))
+"""3. Write a Python program to display 
+the first and last colors from the following list. color_list = ["Red","Green","White" ,"Black"]"""
+# colors_list = ["red", "green", "white", "black"]
+# print(colors_list)
+# print(type(colors_list))
+# print(colors_list[0], colors_list[3])
+""" 7. Write a Python program to check whether a specified value is contained in a group of values. Test Data :
+3 -> [1, 5, 8, 3] : True
+-1 -> [1, 5, 8, 3] : False"""
+# num = [1, 5, 8, 3]
+# g_num = int(input("enter the number :"))
+# for i in num:
+#     if g_num == i:
+#         print("given number is there in our list ")
+#         break
+# else:
+#     print("given number is not in the list ")
+""" 1. Write a Python program to sum all the items in a list. """
+
+
+
+
+
