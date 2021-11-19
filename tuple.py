6299026b9fdd0db696636f5fc8cb8916121d5c8e
rajnanjan <postbox1507@gmail.com>
2021-11-19 09:43:40 +0530

    using the different data types

--- tuple.py
+++ tuple.py
@@ -1,5 +1,9 @@
-"""1. Write a Python program to create a tuple."""
-num = (2, 5, 6, 7, 8)
-print(num)
-print(type(num))
+""" 1. Write a Python program to create a tuple."""
+# num = (2, 5, 6, 7, 8)
+# print(num)
+# print(type(num))
+""" 2. Write a Python program to create a tuple with different data types"""
+values = (1, 45.8, "raj", True)
+print(values)
+
 
