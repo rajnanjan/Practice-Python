4eda691c55011a2b1dbb6244e137fbb0860ad3b6
rajnanjan <postbox1507@gmail.com>
2021-11-20 11:26:59 +0530

    unpack the tuple

--- tuple.py
+++ tuple.py
@@ -3,7 +3,11 @@
 # print(num)
 # print(type(num))
 """ 2. Write a Python program to create a tuple with different data types"""
-values = (1, 45.8, "raj", True)
-print(values)
+# values = (1, 45.8, "raj", True)
+# print(values)
+""" 3. Write a Python program to unpack a tuple in several variables."""
+num = [1, 2, 5, 58]
+(n1, n2, n3, n4) = num
+print(n1, n2, n3, n4)
 
 
