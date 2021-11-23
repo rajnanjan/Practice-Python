66555615c41aee52b6403118a4fe5369865811fd
rajnanjan <postbox1507@gmail.com>
2021-11-23 10:34:14 +0530

    checking the number whether an elements exists

--- tuple.py
+++ tuple.py
@@ -10,13 +10,25 @@
 # (n1, n2, n3, n4) = num
 # print(n1, n2, n3, n4)
 """ 4. Write a Python program to find the repeated items of a tuple."""
+# num = [3, 5, 6, 7, 2, 3, 13, 56, 2, 5, 7]
+# repeated_value = []
+# for i in num:
+#     if num.count(i) > 1:
+#         if i not in repeated_value:
+#             repeated_value.append(i)
+# print(repeated_value)
+""" 6. Write a Python program to check whether an element exists within a tuple."""
 num = [3, 5, 6, 7, 2, 3, 13, 56, 2, 5, 7]
-repeated_value = []
+user_num = int(input("enter the num :"))
 for i in num:
-    if num.count(i) > 1:
-        if i not in repeated_value:
-            repeated_value.append(i)
-print(repeated_value)
+    if user_num == i:
+        print(" given number already exists ")
+        break
+    else:
+        print("given number is not in the tuple ")
+        break
+
+
 
 
 
