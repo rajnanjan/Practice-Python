5d6fccbc93a603ffdf3c3710aa97bebb3b9c421d
rajnanjan <postbox1507@gmail.com>
2021-11-24 21:57:08 +0530

    concatenate dictionaries

--- dictionary.py
+++ dictionary.py
@@ -3,11 +3,26 @@ value."""
 # marks = {"tamil": 56, "english": 89, "maths": 63, "science": 78, "social science": 63}
 # s_marks = sorted(marks)
 # print(s_marks)
+from turtle import update
+
 """ 2. Write a Python script to add a key to a dictionary. 
 Sample Dictionary : {0: 10, 1: 20} 
 Expected Result : {0: 10, 1: 20, 2: 30} """
 
-marks = {"tamil": 56, "english": 89, "maths": 63, "science": 78}
-
-marks["social science"] = 78
-print(marks)
+# marks = {"tamil": 56, "english": 89, "maths": 63, "science": 78}
+#
+# marks["social science"] = 78
+# print(marks)
+""" 3. Write a Python script to concatenate following dictionaries to create a new one. 
+Sample Dictionary : 
+dic1={1:10, 2:20} 
+dic2={3:30, 4:40} 
+dic3={5:50,6:60} 
+Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}"""
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+dic4 = {}
+for i in dic1, dic2, dic3:
+    dic4.update(i)
+print(dic4)
