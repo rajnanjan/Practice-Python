d9d46fe94dbc78dffdf9e15757be566d4f4b5e2a
rajnanjan <postbox1507@gmail.com>
2021-11-27 06:53:15 +0530

    count the number of character in a string

--- string.py
+++ string.py
@@ -1,5 +1,21 @@
 """ 1. Write a Python program to calculate the length of a string. """
+#
+# name = input("enter the name :")
+# print("The length of Name :", len(name))
 
-name = input("enter the name :")
-print("The length of Name :", len(name))
+""" 2. Write a Python program to count the number of characters (character frequency) in a string. 
+Sample String : google.com 
+Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1} """
+
+sample_string = input("Enter the Name :")
+dic = {}
+for ch in sample_string:
+    if ch in dic:
+        dic[ch] += 1
+    else:
+        dic[ch] = 1
+print(dic)
+
+
+
 
