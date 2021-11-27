e549019f862ce2fb616107941aa5dad49b5dfd4d
rajnanjan <postbox1507@gmail.com>
2021-11-27 06:30:43 +0530

    create a dictionary from string key is char value count of letter

--- dictionary.py
+++ dictionary.py
@@ -32,12 +32,32 @@ Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}"""
 # if "pt" in marks:
 #     marks.pop("pt")
 # print(marks)
-""" 7. Write a Python program to print all unique values in a dictionary."""
+""" 7. Write a Python program to print all unique values in a dictionary.
+Sample Data : [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, 
+{"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}] 
+Expected Output : Unique Values: {'S005', 'S002', 'S007', 'S001', 'S009'}
+"""
 # scores = {"raj": 85, "mani": 98, "sachin": 90, "manoj": 85, "sid": 98}
 # u_scores = set()
 # for i in scores.values():
 #     u_scores.add(i)
 # print(u_scores)
+""" 8. Write a Python program to create a dictionary from a string. 
+Note: Track the count of the letters from the string. 
+Sample string : 'w3resource' 
+Expected output: {'3': 1, 's': 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1} """
+
+name = input("enter the name :")
+d_name = {}
+for st in name:
+    if st in d_name:
+        d_name[st] += 1
+    else:
+        d_name[st] = 1
+print(d_name)
+
+
+
 
 
 
