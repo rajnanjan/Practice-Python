ad68dc223705378d7ebd248ed7a61b1f54a0dbf9
rajnanjan <postbox1507@gmail.com>
2021-12-02 10:14:05 +0530

    new branch created

---
+++ crazy_programs.py
@@ -0,0 +1,9 @@
+"""1. Write a Python program which accepts the user's
+first and last name and print them in reverse order with a space between them."""
+
+first_name = input("Enter your first name :")
+last_name = input("Enter the last name :")
+
+print(last_name + " " + first_name)
+
+
