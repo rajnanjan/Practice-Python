9077ad02a4662a3b3e87998ea3369d7e0a08f05a
rajnanjan <postbox1507@gmail.com>
2021-12-19 13:26:02 +0530

    class and objects

---
+++ classes.py
@@ -0,0 +1,43 @@
+# class Car():
+#     wheels = 4
+#     engine = 1
+#     door = 5
+#
+#     def __init__(self, cname, color, price, model, cc):
+#         self.cname = cname
+#         self.color = color
+#         self.model = model
+#         self.cc = cc
+#         self.price = price
+#
+
+#     def moving(self):
+#         return self.cname +" " + self.model + " is moving"
+#
+#
+# maruthi = Car(model="VXI", cc=1200)
+# maruthi = Car(model="s3", cc=2000)
+#
+# print(maruthi.cname)
+# print(Car.wheels)
+# benz.cname = "Tata"
+# print(benz.cname)
+# print(benz.wheels)
+# print(benz.moving())
+# print(maruthi.moving())
+
+class employee:
+    company_name = "CAFS"
+
+    def __init__(self, e_name, e_age, e_dig):
+        self.e_name = e_name
+        self.e_age = e_age
+        self.e_dig = e_dig
+
+    def working(self, company_name=None):
+        print(self.e_name, " is working in ", company_name)
+
+
+raj = employee(e_name="raj", e_age=26, e_dig="RM")
+
+raj.working("cafs")s
