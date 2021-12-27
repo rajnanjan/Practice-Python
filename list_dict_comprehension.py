5632d4e65a9bb2f687f2ed7479f9ea4fff01ef64
rajnanjan <postbox1507@gmail.com>
2021-12-28 05:28:25 +0530

    list and dict comprehension

---
+++ list_dict_comprehension.py
@@ -0,0 +1,67 @@
+'''List Comprehension'''
+
+nums = []
+
+for i in range(1, 21):
+    nums.append(i*2)
+
+print(nums)
+
+nums2 = [i for i in range(1, 21)]
+
+print(nums2)
+
+nums2 = [i*2 for i in range(1, 21)]
+print(nums2)
+
+'''Dict Comprehension'''
+
+names = ['sachin', 'sanju', 'lokesh', 'geetha', 'raaju', 'manoj']
+
+'''Enumerate'''
+dict_2 = {}
+for i, j in enumerate(names):
+    dict_2[i] = j
+
+print(dict_2)
+
+dict1 = {key: value for key, value in enumerate(names)}
+
+print(dict1)
+
+'''Filter , Map, Reduce'''
+# filter()
+# map()
+
+'''Map'''
+# Python program to demonstrate working
+# of map.
+
+# Return double of n
+def addition(n):
+    return n + n
+
+# We double all numbers using map()
+numbers = (1, 2, 3, 4)
+result = map(addition, numbers)
+print(list(result))
+
+'''Filter'''
+# function that filters vowels
+def fun(variable):
+    letters = ['a', 'e', 'i', 'o', 'u']
+    if variable in letters:
+        return True
+    else:
+        return False
+
+
+# sequence
+sequence = ['g', 'e', 'e', 'j', 'k', 's', 'p', 'r']
+
+# using filter function
+filtered = filter(fun, sequence)
+
+print('The filtered letters are:')
+for s in filtered:
+    print(s)
