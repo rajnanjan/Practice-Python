ed7a3295732b8aef2e01fa6d860a478797ebd55b
rajnanjan <postbox1507@gmail.com>
2022-01-03 21:10:30 +0530

    using mapfunction in lamda

--- lambda.py
+++ lambda.py
@@ -5,3 +5,15 @@ num = lambda x: x * 25
 
 print(num)
 print("sum", num(20))
+
+# using lambda with map
+nums = [1, 3, 4, 5, 6, 7, 8]
+
+multi_num = map(lambda x: x * 2, nums)
+print(multi_num)
+for i in multi_num:
+    print(i, end=" ")
+
+
+
+
