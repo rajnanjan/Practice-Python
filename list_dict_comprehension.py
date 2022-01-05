54829c2abeced2d5f1224b39a2f0a22b5d951dbd
rajnanjan <postbox1507@gmail.com>
2022-01-05 06:57:17 +0530

    exercise questions

--- list_dict_comprehension.py
+++ list_dict_comprehension.py
@@ -3,108 +3,49 @@
 # nums = []
 #
 # for i in range(1, 21):
-#     nums.append(i*2)
+#     nums.append(i * 2)
 #
 # print(nums)
-#
-# nums2 = [i for i in range(1, 21)]
-#
-# print(nums2)
-#
-# nums2 = [i*2 for i in range(1, 21)]
-# print(nums2)
-#
-# '''Dict Comprehension'''
-#
-# names = ['sachin', 'sanju', 'lokesh', 'geetha', 'raaju', 'manoj']
-#
-# '''Enumerate'''
-# dict_2 = {}
-# for i, j in enumerate(names):
-#     dict_2[i] = j
-#
-# print(dict_2)
-#
-# dict1 = {key: value for key, value in enumerate(names)}
-#
-# print(dict1)
-# from Tools.scripts.mailerdaemon import x
-
-'''Filter , Map, Reduce'''
-# filter()
-# map()
-
-'''Map'''
-# Python program to demonstrate working
-# of map.
-
-# Return double of n
-# def addition(n):
-#     return n + n
-
-# We double all numbers using map()
-# numbers = (1, 2, 3, 4)
-# result = map(addition, numbers)
-# print(list(result))
-
-'''Filter'''
 
 
-# function that filters vowels
-# def fun(variable):
-#     letters = ['a', 'e', 'i', 'o', 'u']
-#     if variable in letters:
-#         return True
-#     else:
-#         return False
+# filter
+# def is_add(num):
+#     return num % 2 == 1
+# def is_even(num):
+#     return num % 2 == 0
 #
 #
-# sequence
-# sequence = ['g', 'e', 'e', 'j', 'k', 's', 'p', 'r']
+# num = [1, 3, 4, 5, 6, 7, 7, 4, 3, 5, ]
 #
-# # using filter function
-# filtered = filter(fun, sequence)
+# odd_num = filter(is_add, num)
+# for i in odd_num:
+#     print(i, end=", ")
+
+# def square_num(num):
+#     return num * num
 #
-# print('The filtered letters are:')
-# for s in filtered:
-#     print(s)
-# # Python code to demonstrate dictionary
-# comprehension
-
-# # Lists to represent keys and values
-# keys = ['a', 'b', 'c', 'd', 'e']
-# values = [1, 2, 3, 4, 5]
 #
-# # but this line shows dict comprehension here
-# myDict = {k: v for (k, v) in zip(keys, values)}
+# square = map(square_num, num)
+# print(num)
+# print(list(square))
 #
-# # We can use below too
-# # myDict = dict(zip(keys, values))
-#
-# print(myDict)
-
-
-def is_add(num):
-    return num % 2 == 1
-
-
-num = [1, 3, 4, 5, 6, 7, 7, 4, 3, 5, ]
-
-odd_num = filter(is_add, num)
-print(list(odd_num))
-
-
-def square_num(num):
-    return num * num
-
-
-square = map(square_num, num)
-print(num)
-print(list(square))
-
 "1 Find all of the numbers from 1–1000 that are divisible by 8"
+#
+# list1 = [i for i in range(1, 1000) if i % 8 == 0]
+# print(list1)
+" Create a list from the elements of a range from 1200 to 2000 with steps of 130, using list comprehension."
 
-list1 = [i for i in range(1, 1000) if i % 8 == 0]
+list1 = [i for i in range(1200, 2000, 130)]
+
 print(list1)
 
+"Use list comprehension to construct a new list but add 6 to each item."
 
+list2 = [i + 6 for i in list1]
+print(list2)
+
+
+"Using list comprehension, construct a list from the squares of each element in the list."
+
+list3 = [i*i for i in list2]
+print(list3)
